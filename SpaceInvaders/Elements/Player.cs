9aa2692b314818f0dd630d830f7b4ebe24779035
Aprkellas <alex.kellas.relton@gmail.com>
2023-08-21 07:16:27 +0100

    Initial setup

---
+++ SpaceInvaders/Elements/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class Player
+    {
+        public void player_move()
+        {
+
+        }
+    }
+}
