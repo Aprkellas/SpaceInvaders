4c916e4d9821eb62557087f9d9b9b8006304d480
Aprkellas <alex.kellas.relton@gmail.com>
2023-08-21 08:21:30 +0100

    enemy initialization

--- SpaceInvaders/MainWindow.xaml.cs
+++ SpaceInvaders/MainWindow.xaml.cs
@@ -20,16 +20,16 @@ namespace SpaceInvaders
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Player? playerInstance;
 
         public MainWindow()
         {
             InitializeComponent();
-            InitializeGame();
+            InitializePlayer();
+            InitializeEnemies();
 
         }
 
-        public void InitializeGame()
+        public void InitializePlayer()
         {
             Rectangle? playerRectangle = gameGrid.FindName("Player") as Rectangle;
             if (playerRectangle != null)
@@ -38,9 +38,44 @@ namespace SpaceInvaders
             }
         }
 
+        public void InitializeEnemies()
+        {
+            enemies = new Enemy[numRows, numCols];
+
+            for (int row = 0; row < 2; row++) 
+            {
+                for (int col = 0; col < numCols; col++)
+                {
+                    Rectangle enemyRect = new Rectangle
+                    {
+                        Width = enemyWidth,
+                        Height = enemyHeight,
+                        Fill = Brushes.Red
+                    };
+
+                    Grid.SetColumn(enemyRect, col);
+                    Grid.SetRow(enemyRect, row);
+
+                    gameGrid.Children.Add(enemyRect);
+
+                    enemies[row, col] = new Enemy(enemyRect);
+                }
+            }
+        }
+
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
             playerInstance?.MovePlayer(e.Key);
         }
+
+        
+        private Player? playerInstance;
+        private const int numRows = 5; 
+        private const int numCols = 10; 
+        private const int enemyWidth = 40; 
+        private const int enemyHeight = 30;
+
+        private Enemy[,] enemies;
+
     }
 }
